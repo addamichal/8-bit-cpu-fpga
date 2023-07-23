97ab040c09b6962e8a931e7f7b02e81e1adf8edd
Michal Adda <addamichal@gmail.com>
2023-07-23 23:48:09 +0200

    wip

--- program_counter.v
+++ program_counter.v
@@ -9,7 +9,6 @@ module program_counter(
 
 	wire [3:0] adder_out;
 	wire [3:0] dmux_out;
-	wire [3:0] result;
 
 	dmux4 dmux4(
 		.in1(adder_out),
@@ -23,16 +22,14 @@ module program_counter(
 		.clr(clr),
 		.clk(clk),
 		.we(1'd1),
-		.q(result)
+		.q(out)
 	);
 
 	adder4 adder4(
-		.a(result),
+		.a(out),
 		.b(4'd0),
 		.carry_in(ce),
 		.sum(adder_out)
 	);
-
-	assign out = result;
 
 endmodule
