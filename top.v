add7dd7256c113e031549f0e332458f070e0210b
Michal Adda <addamichal@gmail.com>
2023-07-23 12:01:15 +0200

    wip

--- top.v
+++ top.v
@@ -1,14 +1,24 @@
 `include "clock_divider.v"
 `include "uart_send.v"
+`include "seven_seg_ctrl.v"
+`include "seven_seg_hex.v"
+
 
 `default_nettype none
 
 module top(
 	input CLK,
-	input BTN_N, input BTN1, input BTN2, input BTN3,
+	input BTN_N, BTN1, BTN2, BTN3,
 	input RX, output TX,
-	output LED1, output LED2, output LED3, output LED4, output LED5
+	output LED1, LED2, LED3, LED4, LED5,
+	output P1A1, P1A2, P1A3, P1A4, P1A7, P1A8, P1A9, P1A10,
+	input P1B1, P1B2, P1B3, P1B4, P1B7, P1B8, P1B9, P1B10
 );
+
+wire [7:0] seven_segment;
+wire [7:0] dip;
+assign { P1A10, P1A9, P1A8, P1A7, P1A4, P1A3, P1A2, P1A1 } = seven_segment;
+assign { P1B10, P1B9, P1B8, P1B7, P1B4, P1B3, P1B2, P1B1 } = dip;
 
 wire rst = ~BTN_N;
 
@@ -17,18 +27,18 @@ reg done;
 reg busy;
 
 reg [4:0] counter = 0;
-reg [1000: 0] start = "Hello Monika!";
+reg [1000: 0] start = "Hello World!";
 reg [7:0] payload;
 
 always @(posedge done or posedge rst) begin
 	if(rst) begin
 		counter = 0;
 		stopped <= 0;
-	end else if(counter == 4'd13) begin
+	end else if(counter == 4'd12) begin
 		stopped <= 1;
 	end else begin
 		counter <= counter + 1;
-		payload <= start[((12 - counter) + 1) * 8 : (12 - counter) * 8];
+		payload <= start[((11 - counter) + 1) * 8 : (11 - counter) * 8];
 	end
 end
 
@@ -40,6 +50,12 @@ uart_send uart_send(
 	.payload(payload),
 	.busy(busy),
 	.done(done)
+);
+
+seven_seg_ctrl seven_segment_ctrl (
+	.CLK(CLK),
+	.din(counter),
+	.dout(seven_segment)
 );
 
 assign LED1 = done;
